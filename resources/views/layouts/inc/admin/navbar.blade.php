8abbcabedc7db8393deff64c4d5ba1b7f0c94086
Vinay-Mallepalli <mallepallivinay03@gmail.com>
2024-07-31 17:58:44 +0530

    updated

--- resources/views/layouts/inc/admin/navbar.blade.php
+++ resources/views/layouts/inc/admin/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
     <div class="navbar-brand-wrapper d-flex justify-content-center">
         <div class="navbar-brand-inner-wrapper d-flex justify-content-between align-items-center w-100">
-            <a class="navbar-brand brand-logo" href="#">Society Mitra</a>
+            <a class="navbar-brand brand-logo" href="#">Vinay's Housing</a>
             <a class="navbar-brand brand-logo-white" href="#"></a>
             <a class="navbar-brand brand-logo-mini" href="#"></a>
             <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
@@ -11,9 +11,9 @@
     </div>
     <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end flex-grow-1">
         <!-- Heading for larger screens -->
-        <h3 class="navbar-heading navbar-heading-large">Silicon La Vista Co Operative Housing and Commercial Service Society</h3>
+        <h3 class="navbar-heading navbar-heading-large">Co-Operative Housing and Service Society</h3>
         <!-- Heading for smaller screens -->
-        <h3 class="navbar-heading navbar-heading-small">Silicon La Vista</h3>
+        <h3 class="navbar-heading navbar-heading-small">Vinay's Housing</h3>
         <ul class="navbar-nav navbar-nav-right">
             <li class="nav-item nav-profile dropdown" style="margin-right: 10px;">
                 <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" data-bs-toggle="dropdown" id="profileDropdown">
